5a57708576fdeb8898ea2d5e0f06b6e4c7a4234e
Matthew Wind <mtwind2003@gmail.com>
2026-08-09 12:34:32 -0700

    google page and bug fixes

--- src/app/designs/e3/page.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-import { EditorialNoirView } from "./view";
-
-export const metadata: Metadata = { title: "E3 · Editorial Noir" };
-
-export default function EditorialNoir() {
-  return <EditorialNoirView />;
-}
